9b0d63b22ae0de921fc1687515fbc2e6bd8bb061
bob joe <matthew@krupczak.org>
2022-10-14 13:08:30 -0400

    TargetGetter.java implement inverse_haversine

--- app/src/main/java/com/openathena/TargetGetter.java
+++ app/src/main/java/com/openathena/TargetGetter.java
@@ -170,4 +170,106 @@ public class TargetGetter {
         return radDirection;
     }
 
+    double[] inverse_haversine(double lat1, double lon1, double d /*distance*/, double radAzimuth, double alt) {
+        if (d < 0.0d) {
+            return inverse_haversine(lat1, lon1, -1.0d * d, normalize(radAzimuth + Math.PI), alt);
+        }
+
+        final double A = 6378137.0d; // equatorial radius of WGS ellipsoid, in meters
+        final double B = 6356752.3d; // polar radius of WGS ellipsoid, in meters
+
+        lat1 = Math.toRadians(lat1);
+        lon1 = Math.toRadians(lon1);
+
+        // calculate WGS84 radius at lat/lon
+        //     based on: gis.stackexchange.com/a/20250
+        //     R(f)^2 = ( (a^2 cos(f))^2 + (b^2 sin(f))^2 ) / ( (a cos(f))^2 + (b sin(f))^2 )
+        double r = squared(A * A * cos(lat1)) + squared(B * B * sin(lat1)); // numerator
+        r /= squared(A * cos(lat1)) + squared(B * sin(lat1)); // denominator
+        r = Math.sqrt(r); // square root
+        r += alt; // acutal height above or below idealized ellipsoid
+
+        // based on code via github.com/jdeniau
+        double lat2 = asin(sin(lat1) * cos(d / r) + cos(lat1) * sin(d / r) * cos(radAzimuth));
+        double lon2 = lon1 + atan2((sin(radAzimuth) * sin(d/r) * cos(lat1)),(cos(d / r) - sin(lat1) * sin(lat2)));
+
+        double[] returnArr = {Math.toDegrees(lat2), Math.toDegrees(lon2)};
+        return returnArr;
+    }
+
+    // /* Inverse Haversine formula
+    //  * via github.com/jdeniau and math.stackexchange.com/a/3707243
+    //  * given a point, distance, and heading, return the new point (lat lon)
+    //  * a certain distance along the great circle
+    //  *
+    //  * for short distances, this is close to the straight line distance
+    //  */
+    // double[] inverse_haversine_accurate(double lat1, double lon1, double distance, double radAzimuth, double alt) {
+    //     if (distance < 0.0d) {
+    //         return inverse_haversine(lat1, lon1, -1.0d * distance, normalize(radAzimuth + Math.PI), alt);
+    //     }
+    //     double lat2;
+    //     double lon2;
+    //     double r = ???; // WGS84 ellipsoid radius at lat/lon
+    //
+    //     // math.stackexchange.com/a/3707243
+    //     //     runtime is not tenable for mobile devices...
+    //     lat2 = asec(((8 cos(distance/r) sec(lat1) sin6((lon1 - lon2)/2))/(4 sin^4((lon1 - lon2)/2) - 4 sin^2((lon1 - lon2)/2) + tan^2(lat1) + 1) + (4 Math.sqrt(tan^2(lat1) (4 sin^4((lon1 - lon2)/2) - 4 sin^2((lon1 - lon2)/2) - cos^2(distance/r) sec^2(lat1) + tan^2(lat1) + 1)) sin^4((lon1 - lon2)/2))/(4 sin^4((lon1 - lon2)/2) - 4 sin^2((lon1 - lon2)/2) + tan^2(lat1) + 1) - (12 cos(distance/r) sec(lat1) sin^4((lon1 - lon2)/2))/(4 sin^4((lon1 - lon2)/2) - 4 sin^2((lon1 - lon2)/2) + tan^2(lat1) + 1) - 4 cos(distance/r) sec(lat1) sin^2((lon1 - lon2)/2) - (4 Math.sqrt(tan^2(lat1) (4 sin^4((lon1 - lon2)/2) - 4 sin^2((lon1 - lon2)/2) - cos^2(distance/r) sec^2(lat1) + tan^2(lat1) + 1)) sin^2((lon1 - lon2)/2))/(4 sin^4((lon1 - lon2)/2) - 4 sin^2((lon1 - lon2)/2) + tan^2(lat1) + 1) + (2 cos(distance/r) sec(lat1) tan^2(lat1) sin^2((lon1 - lon2)/2))/(4 sin^4((lon1 - lon2)/2) - 4 sin^2((lon1 - lon2)/2) + tan^2(lat1) + 1) + (6 cos(distance/r) sec(lat1) sin^2((lon1 - lon2)/2))/(4 sin^4((lon1 - lon2)/2) - 4 sin^2((lon1 - lon2)/2) + tan^2(lat1) + 1) + 2 cos(distance/r) sec(lat1) + (tan^2(lat1) Math.sqrt(tan^2(lat1) (4 sin^4((lon1 - lon2)/2) - 4 sin^2((lon1 - lon2)/2) - cos^2(distance/r) sec^2(lat1) + tan^2(lat1) + 1)))/(4 sin^4((lon1 - lon2)/2) - 4 sin^2((lon1 - lon2)/2) + tan^2(lat1) + 1) + Math.sqrt(tan^2(lat1) (4 sin^4((lon1 - lon2)/2) - 4 sin^2((lon1 - lon2)/2) - cos^2(distance/r) sec^2(lat1) + tan^2(lat1) + 1))/(4 sin^4((lon1 - lon2)/2) - 4 sin^2((lon1 - lon2)/2) + tan^2(lat1) + 1) - (cos(distance/r) sec(lat1) tan^2(lat1))/(4 sin^4((lon1 - lon2)/2) - 4 sin^2((lon1 - lon2)/2) + tan^2(lat1) + 1) - (cos(distance/r) sec(lat1))/(4 sin^4((lon1 - lon2)/2) - 4 sin^2((lon1 - lon2)/2) + tan^2(lat1) + 1))/(cos^2(distance/r) sec^2(lat1) - tan^2(lat1)))
+    //
+    //     lon2 = lon1 + 2.0d * Math.asin(Math.sqrt(sec(lat1)*sec(lat2)*Math.pow(Math.sin(distance / (2.0d * r)), 2)-sec(lat1)*sec(lat2)*Math.pow(Math.sin((lat2 - lat1) / 2.0d), 2)));
+    // }
+
+    double squared(double val) {
+        return val * val;
+    }
+
+    double sin(double radAngle) {
+        return Math.sin(radAngle);
+    }
+
+    double asin(double radAngle) {
+        return Math.asin(radAngle);
+    }
+
+    double sin2(double radAngle) {
+        return Math.pow(Math.sin(radAngle), 2);
+    }
+
+    double sin4(double radAngle) {
+        return Math.pow(Math.sin(radAngle), 4);
+    }
+
+    double sin6(double radAngle) {
+        return Math.pow(Math.sin(radAngle), 4);
+    }
+
+    double cos(double radAngle) {
+        return Math.cos(radAngle);
+    }
+
+    double cos2(double radAngle) {
+        return Math.pow(Math.cos(radAngle),2);
+    }
+
+
+
+    double tan2(double radAngle) {
+        return Math.pow(Math.tan(radAngle),2);
+    }
+
+    double atan2(double y, double x) {
+        return Math.atan2(y, x);
+    }
+
+    double sec(double radAngle) {
+        return 1.0d / Math.cos(radAngle);
+    }
+
+    double sec2(double radAngle) {
+        return Math.pow((1.0d / Math.cos(radAngle)), 2);
+    }
+
+    double asec(double radAngle) {
+        return Math.acos(1.0d / radAngle);
+    }
 }
